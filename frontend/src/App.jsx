9946e795612eaf6e264e851870339cb718dcbe0c
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-03-18 10:14:15 +1300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import LoginPage from "./components/LoginPage";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import advandedFormat from "dayjs/plugin/advancedFormat";
 
-
+dayjs.extend(relativeTime);
+dayjs.extend(advandedFormat);
 
 function App() {
   return (
