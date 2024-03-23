f6f41df4f4e1223a27bafaa9203f0505e0737643
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-03-23 14:21:57 +1300

    Add search area
    - Add search area
    - Add dark mode

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { IoAdd } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoSearchOutline } from "react-icons/io5";
 
-function Navbar() {
+function Navbar({ darkMode = false, isLogged = false, user = {} }) {
   return (
-    <nav>
-      <div className="mx-auto max-w-main_content h-nav_height text-primary">
+    <nav
+      className={`shadow-sm ${
+        darkMode ? "text-white bg-primary " : "text-primary"
+      } `}
+    >
+      <div className="mx-auto w-4/5 max-w-main_content h-nav_height ">
         <div
           id="nav_content"
-          className="flex flex-row justify-between items-center h-full w-full px-4"
+          className="flex flex-row justify-between items-center h-full w-full"
         >
           <Link to="/" className="flex flex-row gap-4 items-center">
-            <img src="logo.png" alt="logo" className="h-10 rounded-full"  />
+            <img src="logo.png" alt="logo" className="h-10 rounded-full" />
             <div className="text-xl">Hey Mate</div>
           </Link>
-          <div className="flex flex-row gap-4">
-            <Button>Login</Button>
-            <Button style_type="fill">Sign up</Button>
-          </div>
+
+          {!isLogged && (
+            <div className="flex flex-row gap-4">
+              <Button className={`${ darkMode && 'bg-white hover:text-white border-white'}`}>Login</Button>
+              <Button style_type="fill" className={`${ darkMode && 'border-2  border-white hover:border-pink-600'}`} >Sign up</Button>
+            </div>
+          )}
+
+          {isLogged && (
+            <div className="flex flex-row gap-5 items-center">
+              <div className={`flex flex-row items-center border-2 rounded-full pr-1 pl-4 bg-white focus-within:border-primary ${darkMode && 'border-white focus-within:border-white focus-within:text-primary'}  `}>
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className={` w-28 h-8 outline-none focus:w-48 transition-all duration-300 `}
+                />
+                <div className={` w-8 h-7 rounded-full hover:bg-gray-200 flex items-center justify-center ${darkMode && 'text-primary'}`}>
+                  <IoSearchOutline />
+                </div>
+              </div>
+              <div
+                className={`w-7 h-7  text-xl rounded-sm flex flex-row items-center justify-center cursor-pointer  hover:bg-gray-200 ${
+                  darkMode ? "hover:text-primary" : ""
+                }   `}
+              >
+                <IoAdd />
+              </div>
+              <div
+                className={`w-7 h-7  text-xl rounded-sm flex flex-row items-center justify-center hover:bg-gray-200 cursor-pointer ${
+                  darkMode ? "hover:text-primary" : ""
+                } `}
+              >
+                <IoMdNotificationsOutline />
+              </div>
+              <div>
+                <img
+                  src="logo.png"
+                  alt="Avator"
+                  className="h-10 rounded-full cursor-pointer"
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,8 +7,8 @@ import { MdFavoriteBorder } from "react-icons/md";
 function HomePage() {
   return (
     <>
-      <Navbar />
-      <div id="main_content" className="mx-auto max-w-main_content">
+      <Navbar isLogged={true}  darkMode={true} />
+      <div id="main_content" className="mx-auto w-4/5 max-w-main_content">
         <div className="text-3xl">HomePage</div>
         <Link to="/login">Login</Link>
         <MdFavoriteBorder className="text-2xl fill-pink-600" />
