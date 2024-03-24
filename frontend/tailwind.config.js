ffe3062d229161e479c0768f8d13e43b14256cec
Kelvin Jin <jlmjkfd@hotmail.com>
2024-03-25 04:59:37 +1300

    Define some blue colors in tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,11 +6,23 @@ export default {
       colors: {
         primary: "#00467f",
         secondary: "#0080a7",
+        hmblue: {
+          50: "#e5f3ff",
+          100: "#b2dcff",
+          200: "#7fc6ff",
+          300: "#4cafff",
+          400: "#1998ff",
+          500: "#007ee5",
+          600: "#0062b2",
+          700: "#00467f",
+          800: "#002a4c",
+          900: "#00172a",
+        },
       },
       spacing: {
         main_content: "1280px",
-        nav_height: '60px',
-      }
+        nav_height: "60px",
+      },
     },
   },
   plugins: [],
