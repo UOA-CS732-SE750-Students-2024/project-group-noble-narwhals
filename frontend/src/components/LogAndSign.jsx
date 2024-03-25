9fbb9cea85a80bf3ae8b8faea3838a12a16004ce
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-03-25 15:18:35 +1300

    update show password button

--- frontend/src/components/LogAndSign.jsx
+++ frontend/src/components/LogAndSign.jsx
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import { FcGoogle } from "react-icons/fc";
+import { IoIosEye } from "react-icons/io";
+import { IoIosEyeOff } from "react-icons/io";
 
 function LogAndSign({ loginType, switchHandler }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const showPasswordHandler = () => {
+    setShowPassword((preState) => !preState);
+  };
+
   const type = loginType ? "Log in" : "Sign up";
   const contentBlow = loginType
     ? "First time with us?"
     : "Already have an account?";
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    // need to be implemented
+  };
+
   return (
-    <div className="flex flex-col items-center justify-around p-11">
-      <div className="flex flex-col items-center justify-around gap-8">
-        <h1 className="text-5xl font-extrabold">Hey! Mate</h1>
+    <div className="flex flex-col items-center justify-around p-11 w-full">
+      <div className="flex flex-col items-center justify-around gap-6">
+        <h1 className="text-5xl font-extrabold">
+          {loginType ? "Hey!" : "Welcome!"} Mate
+        </h1>
         <img src="logo-no-bg.png" alt="logo" className="w-36 h-36" />
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col items-center gap-9"
+        >
+          <div className="w-full flex flex-row items-center h-10 p-1 pl-4 rounded-full bg-white focus-within:outline outline-2 outline-primary">
+            <input
+              type="text"
+              placeholder="Username"
+              className="outline-none"
+            />
+          </div>
+          <div className="w-full flex flex-row items-center h-10 p-1 pl-4 rounded-full bg-white focus-within:outline outline-2 outline-primary">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="outline-none"
+            />
+            <div
+              onClick={showPasswordHandler}
+              className="flex flex-row items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full"
+            >
+              {showPassword ? <IoIosEye /> : <IoIosEyeOff />}
+            </div>
+          </div>
+          <Button style_type="fill">{type}</Button>
+        </form>
       </div>
-      <form className="flex flex-col items-center">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <Button>{type}</Button>
-      </form>
-      <div>
-        OR
-        <button>{type} with Google</button>
+      <div className="flex flex-col items-center gap-5">
+        <div className="font-bungee text-xl ">OR</div>
+        <Button className="flex flex-row bg-white items-center gap-4 p-4 h-10 mb-9">
+          <FcGoogle className="w-8 h-8" /> {type} with Google
+        </Button>
       </div>
       <div className="flex flex-col items-center">
         {contentBlow}
         <div>
-          <span className="text-primary cursor-pointer" onClick={switchHandler}>{loginType ? "Sign up" : "log in"}</span>{" "}
+          <span
+            className="text-secondary cursor-pointer"
+            onClick={switchHandler}
+          >
+            {loginType ? "Sign up" : "Log in"}
+          </span>{" "}
           here
         </div>
       </div>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -14,7 +14,7 @@ function LoginPage({ ifLogin = true }) {
   return (
     <div
       id="background"
-      className="h-screen w-screen bg-gradient-to-b from-blue-400 to-emerald-300 flex flex-row justify-around relative"
+      className="h-screen w-screen bg-gradient-to-b from-blue-400 to-emerald-300 flex flex-row justify-between relative"
     >
       <LogAndSign loginType={false} switchHandler={switchLoginAndSign} />
       <LogAndSign loginType={true} switchHandler={switchLoginAndSign} />
@@ -23,7 +23,6 @@ function LoginPage({ ifLogin = true }) {
         className={`absolute h-screen w-1/2 bg-primary flex flex-col justify-between p-5 transition-all duration-500 ${
           isLogin ? "left-0 rounded-r-2xl" : "left-1/2 rounded-l-2xl"
         }`}
-        onClick={switchLoginAndSign}
       >
         <div className="font-bungee text-7xl text-white">
           Uplift your mind strengthen your circle
