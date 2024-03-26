30d4f3ec58665dd927e78bf8dc85ee5aa5609733
xinranwangxwan559 <xinran.wang@auckland.ac.nz>
2024-03-26 14:45:45 +1300

    fixed all bugs from 26/3

--- frontend/src/pages/GroupInfo.jsx
+++ frontend/src/pages/GroupInfo.jsx
@@ -5,35 +5,37 @@ import { MdPeople } from "react-icons/md";
 import { IoMdTime } from "react-icons/io";
 import { IoPricetag } from "react-icons/io5";
 import Logo from "../assets/logo-no-bg.png";
+import Button from "../components/Button";
+import { TiDelete } from "react-icons/ti";
 
 function GroupInfo() {
   return (
     <>
       <Navbar />
-      <div className="group-info-container max-w-4xl mx-auto mt-10">
+      <div className="max-w-main_content mx-auto mt-10">
         <div className="header-content flex justify-between items-start mb-4">
           <div className="header-title space-y-2">
             <div className="text-4xl font-bold mb-2">Let us go fishing</div>
             <Link to="/activity" className="text-primary">Activity</Link>
           </div>
           <div className="actions flex flex-col space-y-2">
-            <button className="join-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary py-3 px-16">Join</button>
-            <button className="edit-btn border-2 border-primary text-primary hover:bg-primary hover:text-white py-3 px-16">Edit</button>
+            <Button className="py-3 px-16" style_type="fill">Join</Button>
+            <Button className="py-3 px-16" style_type="border">Edit</Button>
           </div>
         </div>
-
 
         <div className="content">
           <ActivityDetail />
           <MemberList />
           <ApplicantList />
           <Description />
+          <Footer />
         </div>
-
       </div>
     </>
   );
 }
+
 
 function ActivityDetail() {
   return (
@@ -58,147 +60,92 @@ function ActivityDetail() {
     </div>
   );
 }
-
+function Member({ username, role, avatarColor }) {
+  return (
+    <li className="mb-2 flex items-center justify-between">
+      <div className="flex items-center">
+        <div className={`w-8 h-8 rounded-full ${avatarColor} mr-2`}></div>
+        <span>{username}</span>
+        {role && (
+          <span className="bg-gray-200 text-gray-800 rounded-full px-2 ml-2 text-xs">{role}</span>
+        )}
+      </div>
+      <button className="remove-btn text-black px-2  py-1 rounded hover:bg-red-400 transition">
+        <TiDelete size={24}/>
+      </button>
+    </li>
+  );
+}
 function MemberList() {
+  const members = [
+    { username: "username1", role: "Host", avatarColor: "bg-yellow-400" },
+    { username: "username2", avatarColor: "bg-pink-600" },
+    { username: "username3", avatarColor: "bg-blue-400" },
+  ];
+
   return (
     <div className="member-list p-6 mt-6">
-      <h3 className="font-semibold  mb-4">
+      <h3 className="font-semibold mb-4">
         <span>Members</span>
-        <span className=" ml-2 text-gray-500">3/5</span>
+        <span className="ml-2 text-gray-500">3/5</span>
       </h3>
       <ul className="list-none">
-        <li className="mb-2 flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="w-8 h-8 rounded-full bg-yellow-400 mr-2"></div>
-            <span>username</span>
-            <span className="bg-gray-200 text-gray-800 rounded-full px-2 ml-2 text-xs">Host</span>
-          </div>
-        </li>
-        <li className="mb-2 flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="w-8 h-8 rounded-full bg-pink-600 mr-2"></div>
-            <span>username</span>
-          </div>
-
-          <button className="remove-btn text-black px-2 py-1 rounded hover:bg-red-700 transition">X</button>
-
-        </li> <li className="mb-2 flex items-center justify-between">
-          <div className="flex items-center">
-            <div className="w-8 h-8 rounded-full bg-blue-400 mr-2"></div>
-            <span>username</span>
-          </div>
-
-          <button className="remove-btn text-black px-2 py-1 rounded hover:bg-red-700 transition">X</button>
-
-        </li>
+        {members.map((member, index) => (
+          <Member key={index} {...member} />
+        ))}
       </ul>
     </div>
   );
-
 }
-
-
-function ApplicantList() {
+function Applicant({ username, message, avatarColor }) {
   return (
-    <div className="applicant-list p-6 mt-6">
+    <li className="py-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-start space-x-2">
+          <div className={`w-8 h-8 rounded-full ${avatarColor}`}></div>
+          <div>
+            <div className="text-base font-medium">{username}</div>
+            <div className="text-sm text-gray-600">{message}</div>
+          </div>
+        </div>
+        <div className="flex space-x-2">
+          <Button className="mr-2" style_type="fill">Allow</Button>
+          <Button style_type="border">Reject</Button>
+        </div>
+      </div>
+    </li>
+  );
+}
+function ApplicantList() {
+  const applicants = [
+    { username: "username1", message: "Message 1", avatarColor: "bg-blue-800" },
+    { username: "username2", message: "Message 2", avatarColor: "bg-red-400" },
+    { username: "username3", message: "Message 3", avatarColor: "bg-blue-200" },
+    { username: "username4", message: "Message 4", avatarColor: "bg-yellow-400" },
+    { username: "username5", message: "Message 5", avatarColor: "bg-green-400" },
+    { username: "username6", message: "Message 6", avatarColor: "bg-pink-600"},
+    { username: "username7", message: "Message 7", avatarColor: "bg-blue-400"},
+    { username: "username8", message: "Message 8", avatarColor: "bg-yellow-400"},
+    { username: "username9", message: "Message 9", avatarColor: "bg-green-400"}
+  ];
+
+  return (
+    <div className="applicant-list p-6 mt-6 mt-6 max-h-[500px] overflow-y-auto">
       <h3 className="font-semibold mb-4">
         Applicants <span className="text-gray-500">35</span>
       </h3>
       <ul className="divide-y divide-black">
-        {/* Repeat for each applicant */}
-        <li className="py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-start space-x-2">
-              <div className="w-8 h-8 rounded-full bg-blue-800"></div>
-              <div>
-                <div className="text-base font-medium">username</div>
-                <div className="text-sm text-gray-600">Message</div>
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <button className="allow-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary mr-2 ">
-                Allow
-              </button><button className="reject-btn border-2 border-primary text-primary hover:bg-primary hover:text-white ">
-                Reject</button>
-            </div>
-          </div>
-        </li>
-
-        <li className="py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-start space-x-2">
-              <div className="w-8 h-8 rounded-full bg-red-400"></div>
-              <div>
-                <div className="text-base font-medium">username</div>
-                <div className="text-sm text-gray-600">Message</div>
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <button className="allow-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary mr-2 ">
-                Allow
-              </button><button className="reject-btn border-2 border-primary text-primary hover:bg-primary hover:text-white ">
-                Reject</button>
-            </div>
-          </div>
-        </li>
-
-        <li className="py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-start space-x-2">
-              <div className="w-8 h-8 rounded-full bg-blue-200"></div>
-              <div>
-                <div className="text-base font-medium">username</div>
-                <div className="text-sm text-gray-600">Message</div>
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <button className="allow-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary mr-2 ">
-                Allow
-              </button><button className="reject-btn border-2 border-primary text-primary hover:bg-primary hover:text-white ">
-                Reject</button>
-            </div>
-          </div>
-        </li>
-
-        <li className="py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-start space-x-2">
-              <div className="w-8 h-8 rounded-full bg-green-400"></div>
-              <div>
-                <div className="text-base font-medium">username</div>
-                <div className="text-sm text-gray-600">Message</div>
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <button className="allow-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary mr-2 ">
-                Allow
-              </button><button className="reject-btn border-2 border-primary text-primary hover:bg-primary hover:text-white ">
-                Reject</button>
-            </div>
-          </div>
-        </li>
-
-        <li className="py-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-start space-x-2">
-              <div className="w-8 h-8 rounded-full bg-orange-400"></div>
-              <div>
-                <div className="text-base font-medium">username</div>
-                <div className="text-sm text-gray-600">Message</div>
-              </div>
-            </div>
-            <div className="flex space-x-2">
-              <button className="allow-btn bg-primary text-white border-2 border-primary hover:bg-pink-600 hover:text-primary mr-2 ">
-                Allow
-              </button><button className="reject-btn border-2 border-primary text-primary hover:bg-primary hover:text-white ">
-                Reject</button>
-            </div>
-          </div>
-        </li>
+        {applicants.map((applicant, index) => (
+          <Applicant key={index} {...applicant} />
+        ))}
       </ul>
     </div>
   );
 }
+
+
+
+
 
 function Description() {
   return (
@@ -207,26 +154,26 @@ function Description() {
         <h3 className="font-bold text-3xl mb-4"> Description</h3>
         <p>My client is a large multinational business with head offices in Europe, whom have enjoyed a strong presence in the NZ market place for the last 60 years. The business has over 500 staff in three core business units with 7 sites around the country. The business operates in the manufacturing and distribution industry and continues to see growth year on year. They have offices in South and West Auckland so this role could suit someone in either area.</p>
       </div>
-      <div className="footer bg-primary text-white p-4 rounded-b shadow flex">
+    </>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="bg-primary text-white p-4 rounded-b shadow flex">
+      <div className="flex items-center">
         <img src={Logo} alt="Logo" className="h-12 mr-3" />
         <div>
-          <div className="flex items-center mb-1">
-            <p className="font-bold text-lg">Hey Mate</p>
-          </div>
-          <div>
-            <p className="text-sm">Uplift your mind</p>
-            <p className="text-sm">strengthen your circle</p>
-          </div>
+          <p className="font-bold text-lg">Hey Mate</p>
+          <p className="text-sm">Uplift your mind</p>
+          <p className="text-sm">strengthen your circle</p>
         </div>
       </div>
-
-
-
-    </>
-
+    </div>
   );
-
-
 }
+
+
+
 
 export default GroupInfo;
