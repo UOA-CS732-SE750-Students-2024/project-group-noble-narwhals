592aa0f21a384e819477f11cd8e18d531d99844a
Kelvin Jin <jlmjkfd@hotmail.com>
2024-03-27 17:29:31 +1300

    Update Router - Rebuild directory structure   - Move LayoutWithNav to pages/layout   - Rename UserPage to LayoutUserPages and move to pages/layout - ★Create all empty pages,  some of which may use the same name as what you have done. Remember to replace to your code when you deal with conflicts. If it is in a different folder than the one you created, use the directory here and replace it with your own file.

---
+++ frontend/src/pages/SearchPage.jsx
@@ -0,0 +1,3 @@
+export default function SearchPage() {
+  return <div className="mt-16">This is SearchPage</div>;
+}
--- frontend/src/pages/layouts/LayoutWithNav.jsx
+++ frontend/src/pages/layouts/LayoutWithNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "../../components/Navbar";
 
 export default function PageLayout() {
   return (
