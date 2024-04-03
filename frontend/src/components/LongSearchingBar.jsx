ec7c88334337f795e60d04ef0a673ba7f44c2196
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-04-04 00:22:25 +1300

    adjust searching bar's length again

--- frontend/src/components/LongSearchingBar.jsx
+++ frontend/src/components/LongSearchingBar.jsx
@@ -3,7 +3,7 @@ import { IoMdSearch } from "react-icons/io";
 
 const LongSearchingBar = () => {
   return (
-    <div className="flex justify-between items-center w-1/2 bg-white border-2 border-sky-900 h-12 rounded-3xl">
+    <div className="flex justify-between items-center w-3/4 bg-white border-2 border-sky-900 h-12 rounded-3xl">
       <IoMdSearch className="size-10 text-sky-900 ml-0.5" />
       <input className="w-96 h-10 outline-none" />
       <button className="bg-sky-900 text-white rounded-3xl mr-0.5">
