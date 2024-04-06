12e34cf53e611e5e54cc61532514af58767c711d
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2024-04-07 00:33:00 +1300

    Add host avatar

--- frontend/src/components/GalleryCard.jsx
+++ frontend/src/components/GalleryCard.jsx
@@ -15,24 +15,37 @@ const GalleryCard = ({
 }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow-basic hover:bg-slate-100">
-      <div className="flex justify-between items-center m-2">
-        <div className="flex flex-col justify-center h-8">
-          <Link
-            to={`/group/${id}`}
-            className="text-base font-bold text-sky-800 hover:scale-125 hover:text-sky-600"
-          >
-            {title}
-          </Link>
-          <div className="text-sm text-sky-700">{dayNum} days left</div>
+      <div className="flex justify-between">
+        <div className="flex flex-row justify-between">
+          <img
+            key={1}
+            className="inline-block h-10 w-10 rounded-full ring-2 ring-white bg-gray-500 items-center"
+            src={
+              "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            }
+            alt={`Host Avatar`}
+          />
+          <div className="flex flex-col justify-center ml-3">
+            <Link
+              to={`/group/${id}`}
+              className="text-base font-bold text-sky-800 hover:scale-125 hover:text-sky-600"
+            >
+              {title}
+            </Link>
+            <div className="text-sm text-sky-700">{dayNum} days left</div>
+          </div>
         </div>
-        {/* favarite button */}
-        <button className="flex justify-center items-center bg-slate-200 rounded-full p-0 h-8 w-8 hover:scale-110">
-          {isFavorite ? (
-            <MdFavorite size={28} color="red" />
-          ) : (
-            <MdFavoriteBorder size={28} />
-          )}
-        </button>
+
+        <div className="flex items-center">
+          {/* favarite button */}
+          <button className="flex justify-center items-center bg-slate-200 rounded-full p-0 h-8 w-8 hover:scale-110">
+            {isFavorite ? (
+              <MdFavorite size={28} color="red" />
+            ) : (
+              <MdFavoriteBorder size={28} />
+            )}
+          </button>
+        </div>
       </div>
       <div className="text-base text-sky-700 font-thin m-2">
         <p>
@@ -41,7 +54,7 @@ const GalleryCard = ({
             : description}
         </p>
       </div>
-      <div className="flex justify-between items-center m-2">
+      <div className="flex justify-between items-center">
         <AvatarGroup imageSources={imageLink} num={num} />
         <button className="flex justify-center items-center text-sky-800 font-bold border-solid border-2 border-sky-800 rounded-xl w-4 h-6 hover:scale-110">
           Join
