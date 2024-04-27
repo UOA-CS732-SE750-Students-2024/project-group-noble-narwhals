017ca5074d7fffe305a6ace901dfd7dd314d5879
Kelvin Jin <jlmjkfd@hotmail.com>
2024-04-27 12:11:15 +1200

    Get data from database, delete dummy data

--- backend/src/routes/api/group.js
+++ backend/src/routes/api/group.js
@@ -1,101 +1,135 @@
-import express from 'express';
-import Group from '../../models/groupModel.js'
-import { body, validationResult } from 'express-validator';
-import { getGroup } from '../../middleware/entityMiddleware.js';
+import express from "express";
+import Group from "../../models/groupModel.js";
+import { body, validationResult } from "express-validator";
+import { getGroup } from "../../middleware/entityMiddleware.js";
 const router = express.Router();
 
 // get all groups
-router.get('/', async (req, res) => {
-    try {
-        const groups = await Group.find();
-        res.json(groups);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const groups = await Group.find();
+    res.json(groups);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// get all groups for seach page
+router.get("/search", async (req, res) => {
+  try {
+    const groups = await Group.find().populate("groupTags", "name");
+    return res.json(groups);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // get group by id
-router.get('/:id', getGroup, (req, res) => {
-    res.json(res.group);
+router.get("/:id", getGroup, (req, res) => {
+  res.json(res.group);
+});
+
+// get group by keywords
+router.get("/search/:keywords", async (req, res) => {
+  try {
+    const keywords = req.params.keywords
+      .trim()
+      .replace(/\s\s+/g, " ")
+      .replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+      .split(" ");
+    const regex = keywords.map((keyword) => new RegExp(keyword, "i"));
+
+    await Group.find()
+      .populate("groupTags", "name")
+      .then((groups) => {
+        const filteredGroups = groups.filter((group) => {
+          return (
+            regex.some((reg) => reg.test(group.groupName)) ||
+            group.groupTags.some((tag) => keywords.includes(tag.name))
+          );
+        });
+        res.json(filteredGroups);
+      });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // create a new group
-router.post( '/', 
-    [
-        body('groupName').not().isEmpty().withMessage('group name cannot be empty'),
-        body('createDate').optional().isISO8601().toDate(),
-        body('deadlineDate').optional().isISO8601().toDate(),
-        // body('numberOfGroupMember').optional().isInt({ min: 1 }),
-        body('groupDescription').optional().isString(),
-        body('groupType').not().isEmpty().withMessage('group type cannot be empty'),
-        body('maxNumber').optional().isInt({ min: 1 }),
-        //body('groupStatus').optional().isIn(['available', 'closed', 'full']),
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const group = new Group({
-            ...req.body,
-        });
-
-        try {
-            const newGroup = await group.save();
-            res.status(201).json(newGroup);
-        } catch (err) {
-            res.status(400).json({ message: err.message });
-        }
+router.post(
+  "/",
+  [
+    body("groupName").not().isEmpty().withMessage("group name cannot be empty"),
+    body("createDate").optional().isISO8601().toDate(),
+    body("deadlineDate").optional().isISO8601().toDate(),
+    // body('numberOfGroupMember').optional().isInt({ min: 1 }),
+    body("groupDescription").optional().isString(),
+    body("groupType").not().isEmpty().withMessage("group type cannot be empty"),
+    body("maxNumber").optional().isInt({ min: 1 }),
+    //body('groupStatus').optional().isIn(['available', 'closed', 'full']),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-);
 
-
-// update group by id
-router.patch('/update/:id', getGroup, async (req, res) => {
-    // update group properties
-    Object.entries(req.body).forEach(([key, value]) => {
-        res.group[key] = value;
+    const group = new Group({
+      ...req.body,
     });
 
     try {
-        const updatedGroup = await res.group.save();
-        res.json(updatedGroup);
+      const newGroup = await group.save();
+      res.status(201).json(newGroup);
     } catch (err) {
-        res.status(400).json({ message: err.message });
+      res.status(400).json({ message: err.message });
     }
+  }
+);
+
+// update group by id
+router.patch("/update/:id", getGroup, async (req, res) => {
+  // update group properties
+  Object.entries(req.body).forEach(([key, value]) => {
+    res.group[key] = value;
+  });
+
+  try {
+    const updatedGroup = await res.group.save();
+    res.json(updatedGroup);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // delete group by id
-router.delete('/delete/:id', getGroup, async (req, res) => {
-    try {
-        await res.group.remove();
-        res.json({ message: 'Deleted Group' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.delete("/delete/:id", getGroup, async (req, res) => {
+  try {
+    await res.group.remove();
+    res.json({ message: "Deleted Group" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // join group by id
-router.post('/join/:id', getGroup, async (req, res) => {
-    const userId = req.user._id;
+router.post("/join/:id", getGroup, async (req, res) => {
+  const userId = req.user._id;
 
-    // check if user is already in the group
-    if (res.group.groupMembers.includes(userId)) {
-        return res.status(400).json({ message: 'User already in the group' });
-    }
+  // check if user is already in the group
+  if (res.group.groupMembers.includes(userId)) {
+    return res.status(400).json({ message: "User already in the group" });
+  }
 
-    // add user to the group
-    res.group.groupMembers.push(userId);
+  // add user to the group
+  res.group.groupMembers.push(userId);
 
-    try {
-        await res.group.save();
-        res.json({ message: 'User added to the group successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+  try {
+    await res.group.save();
+    res.json({ message: "User added to the group successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
-
-
 
 export default router;
