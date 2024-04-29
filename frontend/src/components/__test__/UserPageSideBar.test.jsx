02bdf59b31841ea804a61d7d7fa7106bc1a46ec7
Tianle Pan <tpan501@aucklanduni.ac.nz>
2024-04-29 15:46:53 +1200

    take navbar.test for reference

---
+++ frontend/src/components/__test__/UserPageSideBar.test.jsx
@@ -0,0 +1,41 @@
+import "@testing-library/jest-dom";
+import { describe, expect, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { MemoryRouter } from "react-router-dom";
+import UserPageSideBar from "../UserPageSideBar";
+import { AuthProvider } from "../../store/AuthContext";
+
+
+let axiosMock;
+
+//before all test, create a new instance of MockAdapter
+beforeAll(() => {
+  axiosMock = new MockAdapter(axios);
+});
+
+//after each test, reset the mock
+afterEach(() => {
+  fireEvent.scroll(window, { target: { scrollY: 0 } });
+  axiosMock.reset();
+});
+
+/**
+ * Test for UserPageSideBar component
+ */
+
+describe("UserPageSideBar component", () => {
+    it("Render <UserPageSideBar> ", () => {
+        const { getByRole } = render(
+        <AuthProvider >
+            <MemoryRouter>
+            <UserPageSideBar />
+            </MemoryRouter>
+        </AuthProvider>
+        );
+    
+        const nav = getByRole("navigation");
+        expect(getByRole("navigation").children.length).toBe(1);
+    });
+});
