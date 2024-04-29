a63a8094cf8e82cf2d2522fa22c55a775fe3bde8
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-04-29 16:58:58 +1200

    Update schema.test.js

--- backend/src/models/__test__/schema.test.js
+++ backend/src/models/__test__/schema.test.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import User from "../userModel";
 import Group from "../groupModel";
+import Tag from "../tagModel";
 
 let mongod;
 
@@ -39,6 +40,29 @@ const group1 = {
 
 const groups = [group1];
 
+const tag1 = {
+  _id: new mongoose.Types.ObjectId("000000000000000000001321"),
+  name: "react",
+  isProfileTag: true,
+  isGroupTag: false,
+};
+
+const tag2 = {
+  _id: new mongoose.Types.ObjectId("000000000000000000001322"),
+  name: "CS732",
+  isProfileTag: false,
+  isGroupTag: true,
+};
+
+const tag3 = {
+  _id: new mongoose.Types.ObjectId("000000000000000000001323"),
+  name: "CS751",
+  isProfileTag: false,
+  isGroupTag: true,
+};
+
+const tags = [tag1, tag2, tag3];
+
 /**
  * Before all tests, create an in-memory MongoDB instance so we don't have to test on a real database,
  * then establish a mongoose connection to it.
@@ -56,12 +80,18 @@ beforeAll(async () => {
 beforeEach(async () => {
   // Drop existing collections
   await mongoose.connection.db.dropDatabase();
+  const collections = await mongoose.connection.db.listCollections().toArray();
+  for (let collection of collections) {
+    await mongoose.connection.db.dropCollection(collection.name);
+  }
 
   const collUser = await mongoose.connection.db.createCollection("users");
   const collGroup = await mongoose.connection.db.createCollection("groups");
+  const collTag = await mongoose.connection.db.createCollection("tags");
 
   await collUser.insertMany(users);
   await collGroup.insertMany(groups);
+  await collTag.insertMany(tags);
 });
 
 /**
@@ -96,4 +126,15 @@ it("Test group schema", async () => {
   expect(groupsFromDb[0].groupStatus).toBe("available");
   expect(groupsFromDb[0]._id.toString()).toBe("000000000000000000000321");
   expect(groupsFromDb[0].groupName).toBe("CS732");
+});
+
+it("Test tag schema", async () => {
+  const tagFromDb = await Tag.find();
+
+  expect(tagFromDb).toBeTruthy();
+  expect(tagFromDb.length).toBe(3);
+
+  expect(tagFromDb[0]._id.toString()).toBe("000000000000000000001321");
+  expect(tagFromDb[1].name.toString()).toBe("CS732");
+  expect(tagFromDb[2].isProfileTag).toBe(false);
 });
