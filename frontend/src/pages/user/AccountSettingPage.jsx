0dabcd6e5bbfb187992f9c8fc678772df82ee22d
Tianle Pan <tpan501@aucklanduni.ac.nz>
2024-05-03 17:03:58 +1200

    fix bug

--- frontend/src/pages/user/AccountSettingPage.jsx
+++ frontend/src/pages/user/AccountSettingPage.jsx
@@ -174,6 +174,8 @@ function AccountSettingsPage() {
     if (isPasswordChanged && password.length < 8) {
       alert('Password must be at least 8 characters long.');
       return;
+    }else{
+      updatedUserData.password = password;
     }
   
 
