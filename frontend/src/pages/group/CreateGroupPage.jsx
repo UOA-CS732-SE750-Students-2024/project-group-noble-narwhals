23e01c9c2f83e2cb4396f5ff7beb1c2a536aa940
ryanwooa <119087910+ryanwooa@users.noreply.github.com>
2024-05-08 21:53:20 +1200

    Update CreateGroupPage.jsx

--- frontend/src/pages/group/CreateGroupPage.jsx
+++ frontend/src/pages/group/CreateGroupPage.jsx
@@ -269,6 +269,7 @@ function CreatGroupPage() {
               Due Date
             </label>
             <input
+              data-testid="dueDate"
               min={new Date().toISOString().split("T")[0]}
               type="date"
               className="border-2 border-primary w-3/5 sm:w-2/5 rounded-full h-9 px-4"
